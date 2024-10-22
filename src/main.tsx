81992c497ca3597865773ceb70b54f8bbb5241dd
nbhagat <nbhagat@bol.com>
2024-10-22 13:29:58 +0200

    Initiate vite-react-ts project

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
